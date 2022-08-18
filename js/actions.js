546f3a1e4cd6a9c7bd78c9fa77e7cc0ffb3029c2
Felroi Francis <Felroi@Felrois-MacBook-Air.local>
2022-08-18 17:45:57 +0100

    updates

--- js/actions.js
+++ js/actions.js
@@ -47,3 +47,62 @@ function submitEmail(event) {
 
   return false;
 }
+
+function submitContactUs(event) {
+  event.preventDefault();
+  alert("The form was submitted");
+
+  const API_URL = "https://api.sendgrid.com/v3/mail/send";
+  const NEWSLETTER_ID = "328b3f49-b814-4379-b2e9-b54cd01138cc";
+  const API_KEY =
+    "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";
+
+  const prepSub = function (email) {
+    return JSON.stringify({
+      to: [{ email: email }],
+      from: { email: "test@example.com" },
+      subject: "Sending with SendGrid is Fun",
+      content: [
+        {
+          type: "text/plain",
+          value: "and easy to do anywhere, even with cURL",
+        },
+      ],
+    });
+  };
+
+  const { value: email } = document.querySelector("#email_address");
+  const form = document.querySelector("form.contactus-form");
+  const loading = document.querySelector("#form-loader");
+  const formLoaded = document.querySelector("#form-loaded");
+  const formErrored = document.querySelector("#form-errored");
+
+  form.classList.add("hidden");
+  loading.classList.remove("hidden");
+
+  const fetchCall = fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    data: prepSub(email),
+  });
+
+  fetchCall
+    .then(function (resp) {
+      console.log("back from send grid");
+      console.log(resp);
+      loading.classList.add("hidden");
+      formLoaded.classList.remove("hidden");
+      formErrored.classList.add("hidden");
+    })
+    .catch(function (rej) {
+      form.classList.remove("hidden");
+      loading.classList.add("hidden");
+      formLoaded.classList.add("hidden");
+      formLoaded.classList.remove("hidden");
+    });
+
+  return false;
+}
