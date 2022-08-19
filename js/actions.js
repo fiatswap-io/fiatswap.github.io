f2d37771aa585e61839a331c0cbbc94d1ede4361
Felroi Francis <Felroi@Felrois-MacBook-Air.local>
2022-08-19 17:22:11 +0100

    contact us submission updates

--- js/actions.js
+++ js/actions.js
@@ -49,30 +49,27 @@ function submitEmail(event) {
 }
 
 function submitContactUs(event) {
-  event.preventDefault();
-  alert("The form was submitted");
-
-  const API_URL = "https://api.sendgrid.com/v3/mail/send";
-  const NEWSLETTER_ID = "328b3f49-b814-4379-b2e9-b54cd01138cc";
-  const API_KEY =
-    "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";
+  const API_URL = "https://api.sendgrid.com/v3/marketing/contacts";
+  const CONTACTUS_ID = "8178cf41-6377-44aa-b54d-18767e5c4fa7";
+  const API_KEY = "";
 
   const prepSub = function (email) {
     return JSON.stringify({
-      to: [{ email: email }],
-      from: { email: "test@example.com" },
-      subject: "Sending with SendGrid is Fun",
-      content: [
+      list_ids: [CONTACTUS_ID],
+      contacts: [
         {
-          type: "text/plain",
-          value: "and easy to do anywhere, even with cURL",
+          email,
+          full_name,
+          message,
         },
       ],
     });
   };
 
-  const { value: email } = document.querySelector("#email_address");
-  const form = document.querySelector("form.contactus-form");
+  const { value: email } = document.querySelector("#email");
+  const { value: full_name } = document.querySelector("#name");
+  const { value: message } = document.querySelector("#message");
+  const form = document.querySelector("form.subscribe-form");
   const loading = document.querySelector("#form-loader");
   const formLoaded = document.querySelector("#form-loaded");
   const formErrored = document.querySelector("#form-errored");
@@ -81,18 +78,16 @@ function submitContactUs(event) {
   loading.classList.remove("hidden");
 
   const fetchCall = fetch(API_URL, {
-    method: "POST",
+    method: "PUT",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${API_KEY}`,
+      authorization: `Bearer ${API_KEY}`,
     },
-    data: prepSub(email),
+    body: prepSub(email),
   });
 
   fetchCall
     .then(function (resp) {
-      console.log("back from send grid");
-      console.log(resp);
       loading.classList.add("hidden");
       formLoaded.classList.remove("hidden");
       formErrored.classList.add("hidden");
